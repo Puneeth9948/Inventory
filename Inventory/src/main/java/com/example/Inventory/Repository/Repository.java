fb7e7ba06fdb8d795fb6266cdd9eb5aa0c6d1d11
Puneeth9948 <pxa32960@ucmo.edu>
2025-06-12 21:02:29 -0500

    Delete Inventory directory

--- Inventory/src/main/java/com/example/Inventory/Repository/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Inventory.Repository;
-
-import com.example.Inventory.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-@org.springframework.stereotype.Repository
-public interface Repository extends JpaRepository<Product, Long> {
-}
